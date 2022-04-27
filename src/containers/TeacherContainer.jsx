0d742daaf8a2f6833350d5a2384ece60fd5b94a0
mathias labora martirena <mathiaslabora@gmail.com>
2022-04-27 09:54:38 -0300

    commit antes merge

--- src/containers/TeacherContainer.jsx
+++ src/containers/TeacherContainer.jsx
@@ -1,4 +1,4 @@
-
+import React from 'react'
 import { useDispatch, useSelector } from "react-redux";
 import { teacherAction } from "../actions/teacherAction";
 import { TeacherForm } from "../components/TeacherForm";
